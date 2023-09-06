19c124c5537162724cafb8f042fb8defa5c5b05a
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-09-06 13:36:22 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,32 @@
+//Вариант 1
+
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  
+  return { min: min, max: max, sum: sum };
+}
+
+//Вариант 2
+
+function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = arr.reduce((a, b) => a + b, 0);
+
+  return { min: min, max: max, sum: sum };
 }
 
 function summElementsWorker(...arr) {
