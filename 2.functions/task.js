f9cda608211771475bfa89b00e719ef9d6403063
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-09-06 19:48:21 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,12 +1,11 @@
-//Вариант 1
+//задача 1 - Вариант 1
 
 function getArrayParams(...arr) {
-
   let min = arr[0];
   let max = arr[0];
   let sum = 0;
 
-  for (let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
 
     if (arr[i] < min) {
@@ -15,36 +14,84 @@ function getArrayParams(...arr) {
       max = arr[i];
     }
   }
-  
+
+  if (arr.length === 0) {
+    return 0;
+  }
+
   return { min: min, max: max, sum: sum };
 }
 
-//Вариант 2
+//задача 1 - Вариант 2
 
 function getArrayParams(...arr) {
   let min = Math.min(...arr);
   let max = Math.max(...arr);
   let sum = arr.reduce((a, b) => a + b, 0);
 
+  if (arr.length === 0) {
+    return 0;
+  }
+
   return { min: min, max: max, sum: sum };
 }
 
-function summElementsWorker(...arr) {
+//задача 2
 
+function summElementsWorker(...arr) {
+  return arr.reduce((a, b) => a + b, 0);
+  if (arr.length === 0) {
+    return 0;
+  }
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  if (arr.length === 0) {
+    return 0;
+  }
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += sumEvenElement;
+    } else {
+      sumOddElement += sumOddElement;
+    }
+  }
+
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return sumOddElement - sumEvenElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++)
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
+//задача 3
 
+function makeWork(arrOfArr, func) {
+  
 }
