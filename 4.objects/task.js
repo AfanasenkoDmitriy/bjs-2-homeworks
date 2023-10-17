9282ddd51dbe25d1da0f7192fe5f7031177d7e03
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-10-17 18:19:20 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,23 @@
 function Student(name, gender, age) {
-  
+    this.name;
+    this.gender;
+    this.age;
+    this.marks;
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    const student = new Student(Student.name, Student.gender, Student.age)
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    Student.addMarks = marks;
 }
 
 Student.prototype.getAverage = function () {
-  
+    Student.getAverage = '';
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    Student.exclude = reason;
 }
