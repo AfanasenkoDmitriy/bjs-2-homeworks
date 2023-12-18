b2b09eb127baf297153bb1dfa8659631ada98fc2
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-12-18 22:52:04 +0200

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,26 @@
-﻿
+﻿function parseCount(number) {
+  try {
+    this.number = Number.parseFloat(number);
+    return number;
+  } catch (error) {
+    if (number === NaN) {
+      return new Error("Невалидное значение");
+    }
+  } finally {
+    return Math.floor(number);
+  }
+}
+
+function validateCount(number) {
+  this.number = number;
+  let result = 0;
+  try {
+    result = parseCount(number);
+  } catch (error) {
+    if (result === NaN) {
+      return new Error("Невалидное значение");
+    }
+  } finally {
+    return result.toFixed(2);
+  }
+}
