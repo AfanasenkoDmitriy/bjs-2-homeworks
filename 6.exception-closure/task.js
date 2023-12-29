1d2630a862ef7dbf897337ad3b25692104b9a1b8
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-12-29 14:50:41 +0200

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,20 +1,16 @@
-﻿
+﻿// Задача №1
+
 function parseCount(number) {
-    this.number = number;
-    if (!isNaN(number)) {
-        return parseFloat(number);
-    } else {
-        throw new Error("невалидное значение");
-    }
+  if (!isNaN(number)) {
+    return parseFloat(number);
+  } else {
+    throw new Error("Невалидное значение");
+  }
 }
 
 function validateCount(number) {
-  try {
-    this.number = parseCount(number);
-    if (!isNaN(number)) {
-      return Number(number);
-    }
-  } catch (error) {
-    return error;
-  }
+    return parseCount(number);
 }
+
+// Задача №2
+
