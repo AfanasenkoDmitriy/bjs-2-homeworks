75a3e5f273e1b69a7aa0a12d113899900f40cb3e
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2024-01-02 16:50:00 +0200

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -9,8 +9,52 @@ function parseCount(number) {
 }
 
 function validateCount(number) {
+  try {
     return parseCount(number);
+  } catch (error) {
+    return error;
+  }
 }
 
 // Задача №2
 
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    } else {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  }
+
+  get perimeter() {
+    const p = this.a + this.b + this.c;
+    return p;
+  }
+
+  get area() {
+    let s = (this.a + this.b + this.c) / 2;
+    return Number(
+      Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3)
+    );
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    let trg = new Triangle(a, b, c);
+    return trg;
+  } catch (error) {
+    let err = {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+    return err;
+  }
+}
